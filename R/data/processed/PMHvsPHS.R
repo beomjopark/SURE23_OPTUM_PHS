944ba70fd8c17470faf4a690f60299d2a2a3e58e
Hugo Baca <86990271+HUGOXBACA@users.noreply.github.com>
2023-07-01 18:57:18 -0400

    Broke down by race as much as possble, basically three categories: community & support, transit factors, and mental health EDA. Lots of scatter by raw values and PHS vs Factor by Race.

--- R/data/processed/PMHvsPHS.R
+++ R/data/processed/PMHvsPHS.R
@@ -2,14 +2,43 @@ library(tidyverse)
 library(ggplot2)
 #read our cleaned up data into the env
 data_analytic<- readRDS("data/interim/analytic2023_c1.rds")
-data_phs_raw <- data_analytic %>% select("v005_rawvalue")
-data_pmh_raw <-data_analytic %>% select("v024_rawvalue")
-#we want to plot poor mental health and PHS
-ggplot(data_analytic, aes(x = v005_rawvalue, y = v024_rawvalue))+
-  geom_point(color="white")+
+
+#we want to plot poor mental health days and PHS
+#PMHD(v042_rawvalue):Average number of mentally unhealthy days reported in past 30 days (age-adjusted).
+ggplot(data_analytic, aes(y = v005_rawvalue, x = v042_rawvalue))+
+  geom_point(color="green", alpha =0.5)+
+  geom_smooth()+
+  xlab(paste("Average number of mentally unhealthy days reported in past 30 days (age-adjusted).")) +
+  ylab("Preventable Hospital Stays")+
   theme(panel.background = element_rect(fill = "black"),
-           plot.background = element_rect(fill = "black"),
-           panel.grid.major = element_line(color = "white", size = 0.1),
-           panel.grid.minor = element_line(color = "white", size = 0.05),
-           text = element_text(color = "white"))
+        plot.background = element_rect(fill = "black"),
+        panel.grid.major = element_line(color = "white", size = 0.1),
+        panel.grid.minor = element_line(color = "white", size = 0.05),
+        text = element_text(color = "white"))
+
+#phs vs frequent mental distress
+#FMD(v145):Percentage of adults reporting 14 or more days of poor mental health per month (age-adjusted).
+ggplot(data_analytic, aes(y = v005_rawvalue, x = v145_rawvalue))+
+  geom_point(color="green",alpha =0.5)+
+  geom_smooth() +
+  xlab(paste("Ratio of household income at the 80th percentile to income at the 20th percentile.")) +
+  ylab("Preventable Hospital Stays") +
+  theme(panel.background = element_rect(fill = "black"),
+        plot.background = element_rect(fill = "black"),
+        panel.grid.major = element_line(color = "white", size = 0.1),
+        panel.grid.minor = element_line(color = "white", size = 0.05),
+        text = element_text(color = "white"))
+
+#phs vs poor or fair health 
+#PFH(v002):Percentage of adults reporting fair or poor health (age-adjusted).
+ggplot(data_analytic, aes(y = v005_rawvalue, x = v002_rawvalue))+
+  geom_point(color="green", alpha =0.5)+
+  geom_smooth()+
+  xlab(paste("Percentage of adults reporting fair or poor health (age-adjusted)")) +
+  ylab("Preventable Hospital Stays") +
+  theme(panel.background = element_rect(fill = "black"),
+        plot.background = element_rect(fill = "black"),
+        panel.grid.major = element_line(color = "white", size = 0.1),
+        panel.grid.minor = element_line(color = "white", size = 0.05),
+        text = element_text(color = "white"))
 
