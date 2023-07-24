0093a9e49269c07045e8f55912f625f928dbf2f7
Hugo Baca <86990271+HUGOXBACA@users.noreply.github.com>
2023-07-23 23:26:08 -0400

    updated to app

--- ShinyApp/ShinnyApp.R
+++ ShinyApp/ShinnyApp.R
@@ -4,7 +4,7 @@ rsconnect::setAccountInfo(name='bdlp5f-hugo-baca',
                           token='8E15EBFA411A95DC922A43F3122CBD34',
                           secret='Wh7lRaJZV7eqScEk1U6gOWK9ibH0oAb04zk84Vyc')
 
-
+rsconnect::deployApp('../test/ShinyApp/ShinnyApp.R')
 ui <- fluidPage(
   "Healthcare Optimization for Preventable Stays"
 )
